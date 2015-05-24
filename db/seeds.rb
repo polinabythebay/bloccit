4de48e025369f426a39fa83d71a973bc09a0ba39
psoshnin <psoshnin@wellesley.edu>
2015-05-24 13:54:00 -0700

    checkpoint 34 complete, topics

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,17 +12,49 @@ require 'faker'
 end
 users = User.all
 
-user = User.first
-user.skip_reconfirmation!
-user.update_attributes!(
-  email: 'psoshnin@gmail.com',
+#Create Admin
+admin = User.new(
+  name: 'Admin User',
+  email: 'admin@example.com',
+  password: 'helloworld',
+  role: 'admin'
+  )
+admin.skip_confirmation!
+admin.save!
+
+#Create a moderator
+moderator = User.new(
+  name: 'Moderator User',
+  email: 'moderator@example.com',
+  password: 'helloworld',
+  role: 'moderator'
+  )
+moderator.skip_confirmation!
+moderator.save!
+
+#Create a member
+member = User.new(
+  name: 'Member User',
+  email: 'member@example.com',
   password: 'helloworld'
   )
+member.skip_confirmation!
+member.save!
+
+#Create topics
+15.times do
+  Topic.create!(
+    name: Faker::Lorem.sentence,
+    description: Faker::Lorem.paragraph
+    )
+end
+topics = Topic.all
 
 # Create Posts
 50.times do 
   Post.create!(
     user:   users.sample,
+    topic: topics.sample,
     title: Faker::Lorem.sentence,
     body: Faker::Lorem.paragraph
     )
@@ -42,4 +74,5 @@ puts "Seed finished!"
 puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+puts "#{Topic.count} topics created"
 
