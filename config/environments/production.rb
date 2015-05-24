df319bdce2b7dc6fd72a155f5cce8546e5cae085
psoshnin <psoshnin@wellesley.edu>
2015-05-24 14:12:31 -0700

    modify production env for send grid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,4 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   
   config.action_mailer.default_url_options = { host: 'polina-bloccit.herokuapp.com' }
- end
 end
