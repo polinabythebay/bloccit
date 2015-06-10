9d703f8105aeedc256a18fb844f60da0eaf6e453
psoshnin <psoshnin@wellesley.edu>
2015-06-10 16:27:52 -0700

    add images to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606031612) do
+ActiveRecord::Schema.define(version: 20150610204514) do
 
   create_table "advertisements", force: true do |t|
     t.string   "title"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150606031612) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
