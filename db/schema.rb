9ccbcb9fc326de49664a2cf72cd9522ab9c1ae8e
psoshnin <psoshnin@wellesley.edu>
2015-06-15 18:22:49 -0700

    add voting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606031612) do
+ActiveRecord::Schema.define(version: 20150616005303) do
 
   create_table "advertisements", force: true do |t|
     t.string   "title"
@@ -39,6 +39,8 @@ ActiveRecord::Schema.define(version: 20150606031612) do
     t.datetime "updated_at"
     t.integer  "user_id"
     t.integer  "topic_id"
+    t.string   "image"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -95,5 +97,16 @@ ActiveRecord::Schema.define(version: 20150606031612) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   add_index "users", ["user_id"], name: "index_users_on_user_id"
+
+  create_table "votes", force: true do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
+  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
---
+++ spec/models/vote_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Vote, type: :model do
+
+  describe "validations" do
+    describe "value validation" do
+      it "allows 1 as valid value" do
+        vote = Vote.new(value: 1)
+        result = vote.save
+        expect(result).to eq(true)
+      end
+
+      it "allows -1 as valid value" do
+        vote = Vote.new(value: -1)
+        result = vote.save
+        expect(result).to eq(true)
+      end
+
+      it "does not allow 0 as valid value" do
+        vote = Vote.new(value: 0)
+        result = vote.save
+        expect(result).to eq(false)
+      end
+    end
+  end
+end
