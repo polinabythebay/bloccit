df0f35b03d340ad9aae38ae62fbfd74f2804c8c0
psoshnin <psoshnin@wellesley.edu>
2015-06-22 18:34:19 -0700

    assignment in progress

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,8 @@ class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
   scope :visible_to, -> (user) { user ? all : where(public: true) }
+
+  scope :publicly_viewable, -> where(public: true)
+  scope :privately_viewable, -> where(public: false)
+  
 end
