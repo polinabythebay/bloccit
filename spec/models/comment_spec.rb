31a970c9b182c6c13215154e55e1ac4d02cee10b
psoshnin <psoshnin@wellesley.edu>
2015-06-27 16:01:37 -0700

    remove tests in comment spec

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,48 +1,5 @@
 require 'rails_helper'
 
 describe Comment do
-  include TestFactories
-  describe "after_create" do
-    before do
-      @post = associated_post
-      @user = authenticated_user(email_favorites: true)
-      @other_user = authenticated_user
-      @comment = Comment.new(body: 'My comment is really great', post: @post, user: @other_user)
-    end
 
-    context "with user's permission" do
-
-      xit "sends an email to users who have favorited the post" do
-        favorite = @user.favorites.create(post: @post)
-
-        allow(FavoriteMailer)
-          .to receive(:new_comment)
-          .with(@user, @post, @comment)
-          .and_return(double(deliver_now: true))
-
-        expect(FavoriteMailer)
-          .to receive(:new_comment)
-
-        @comment.save
-      end
-
-     xit "does not send emails to users who haven't favorited a post" do
-        expect(FavoriteMailer)
-          .not_to receive(:new_comment)
-        @comment.save
-      end 
-    end
-
-    context "without permission" do
-      #there are a lot of befores going on in this spec in many places
-      before { @user.update_attribute(:email_favorites, false) }
-
-      it "does not send emails, even to users who have favorited" do
-        @user.favorites.where(post: @post).create
-        expect(FavoriteMailer)
-          .not_to receive(:new_comment)
-        @comment.save
-      end
-    end
-  end
 end
