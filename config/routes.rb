ad941c880ed75d189a42106e5d26617e58722283
psoshnin <psoshnin@wellesley.edu>
2015-07-08 19:25:51 -0700

    complete popular posts checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
 
   resources :topics do 
     resources :posts, except: [:index] 
   end
 
-  resources :posts, only: [] do
+  resources :posts, except: [:index], controller: 'topics/posts'
+  
+  resources :posts, only: [:index] do
     resources :comments, only: [:create, :destroy]
     resources :favorites, only: [:create, :destroy]
 
